bf251d9dbebb2302124fffc67c0885ab4011589e
cmasten <96593076+cmasten@users.noreply.github.com>
2025-06-04 19:42:18 -0400

    Add Blazor Server project

---
+++ src/InsightLog.Blazor/Program.cs
@@ -0,0 +1,29 @@
+using InsightLog.Blazor.Services;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient("Api", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://localhost:5001/");
+});
+
+builder.Services.AddScoped<JournalEntryService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
